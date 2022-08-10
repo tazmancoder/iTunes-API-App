1ea06f5dd984db1213776b1acc15963ed3f895ef
Mark J Perryman <appledad05@gmail.com>
2022-08-10 07:10:31 -0600

    Initial Commit

---
+++ iTunes Seach/iTunes_SeachApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunes_SeachApp.swift
+//  iTunes Seach
+//
+//  Created by Mark Perryman on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct iTunes_SeachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
